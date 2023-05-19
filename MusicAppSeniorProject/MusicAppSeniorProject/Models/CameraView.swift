c8e132feff186a80fdbfe4cf3907ca2b467a6627
FRCroboSam <s-wangsam@bsd405.org>
2023-05-18 23:39:34 -0700

    Go to new screen after taking photo
    this took too long

--- MusicAppSeniorProject/MusicAppSeniorProject/Models/CameraView.swift
+++ MusicAppSeniorProject/MusicAppSeniorProject/Models/CameraView.swift
@@ -9,7 +9,7 @@ struct CameraView: View {
  
     private static let barHeightFactor = 0.15
     @State var move: Bool = false
-    
+    @State var previousPhotoAssetsCount = 0
     var body: some View {
         
         NavigationStack {
@@ -44,14 +44,21 @@ struct CameraView: View {
 //            .navigationBarHidden(true)
             .ignoresSafeArea()
             .statusBar(hidden: true)
-            .onReceive(NotificationCenter.default.publisher(for: Notification.Name("Took a photo"))) { _ in
-                move.toggle()
-                print("Photo NUMBERS: " + String(model.photoCollection.photoAssets.count))
-            }
+//            .onReceive(NotificationCenter.default.publisher(for: Notification.Name("Took a photo"))) { _ in
+//                move.toggle()
+//                print("Photo NUMBERS: " + String(model.photoCollection.photoAssets.count))
+//            }
         }.navigationDestination(isPresented: $move) {
             if(model.photoCollection.photoAssets.first != nil){
                 PhotoView(asset: model.photoCollection.photoAssets.first!, cache: model.photoCollection.cache)
             }
+        }.onChange(of: model.photoCollection.photoAssets.count) { newCount in
+            if newCount > previousPhotoAssetsCount {
+                // New photo captured, update UI or perform any necessary actions
+                move = true
+                print("Photo NUMBERS: \(model.photoCollection.photoAssets.count)")
+            }
+            previousPhotoAssetsCount = newCount
         }
     }
     
@@ -79,7 +86,7 @@ struct CameraView: View {
             
             Button {
                 model.camera.takePhoto()
-
+                previousPhotoAssetsCount = model.photoCollection.photoAssets.count
             } label: {
                 Label {
                     Text("Take Photo")
